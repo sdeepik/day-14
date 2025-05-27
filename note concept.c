8f330c7ddee60f9d0c04806803b4c406779eae36
S.deepika <deepikadeeps416@gmail.com>
2025-05-27 03:25:57 -0700

    Create note concept.c

---
+++ note concept.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+ 
+if(a<=b/2)
+                  {
+                      printf("%d",a);
+                      
+                  }   
+                  else{
+                      printf("%d",b/2);
+                  }
+    return 0;
+}
